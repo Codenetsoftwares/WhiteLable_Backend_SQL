5a478002b8a45f9f57dbb54025be36549216ef8d
Gulshan-codenet-softwares <gulshan.codenet@gmail.com>
2024-10-24 17:57:11 +0530

    add manage urls

--- helper/manageUrl.js
+++ helper/manageUrl.js
@@ -1,8 +1,18 @@
+
 import dotenv from "dotenv";
 dotenv.config();
 
-export const API_URL = () => {
-  return process.env.NODE_ENV === "production"
-    ? process.env.PRODUCTION_API_URL
-    : process.env.API_URL;
+export const API_URL = () => { 
+  if (process.env.NODE_ENV === "production") {
+    return {
+      whiteLabelUrl: process.env.WHITE_LABEL_URL ,
+      lotteryUrl: process.env.LOTTERY_URL 
+    };
+  } else {
+    return {
+      whiteLabelUrl: process.env.DEV_WHITE_LABEL_URL || "http://localhost:8000",
+      lotteryUrl: process.env.DEV_LOTTERY_URL || "http://localhost:8080",
+      
+    };
+  }
 };
