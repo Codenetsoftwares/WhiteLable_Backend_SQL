0dd579e34a782c2c576f2566ae3fb02a7905f0db
riktadeyy <rikta.dey.cns@gmail.com>
2024-11-29 15:22:01 +0530

    marge branch feature-28/live-old-backupdata

--- routes/lotteryGame.route.js
+++ routes/lotteryGame.route.js
@@ -1,11 +1,13 @@
 import { getBetHistoryP_L, getLotteryBetHistory, getLotteryP_L, lotteryMarketAnalysis } from "../controller/lotteryGame.controller.js";
+import customErrorHandler from "../helper/customErrorHandler.js";
+import { validateGetExternalLotteryP_L } from "../schema/commonSchema.js";
 
 export const lotteryGameModule = (app) => {
     app.post('/api/get-lottery-bet-history/:userName', getLotteryBetHistory);
 
     app.post('/api/get-lottery-marketAnalysis/:marketId', lotteryMarketAnalysis);
 
-    app.get('/api/lottery-profit-loss/:userName', getLotteryP_L);
+    app.get('/api/lottery-profit-loss/:userName', validateGetExternalLotteryP_L, customErrorHandler, getLotteryP_L);
 
     app.get('/api/lottery-betHistory-profitLoss/:userName', getBetHistoryP_L);
 
