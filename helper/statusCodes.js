298c1adef3e856a6953bab755a4473e05fb0f6aa
riktadeyy <rikta.dey.cns@gmail.com>
2024-12-11 18:04:38 +0530

    create feature of master book user book into one function

--- helper/statusCodes.js
+++ helper/statusCodes.js
@@ -1,11 +1,11 @@
 export const statusCode = {
-    success: 200,
-    create: 201,
-    badRequest: 400,
-    notFound: 404,
-    internalServerError: 500,
-    inActive : 403,
-    unauthorize : 401,
-    exist : 409
-  };
-  
+  success: 200,
+  create: 201,
+  badRequest: 400,
+  notFound: 404,
+  internalServerError: 500,
+  inActive: 403,
+  unauthorize: 401,
+  exist: 409,
+  forbidden: 403
+};
